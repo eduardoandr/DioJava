84ff9e373a65c1c07291abe7d589e2a8406805f8
Eduardo <edu3839@hotmail.com>
2022-01-07 13:35:41 -0300

    Programa básico em java

---
+++ src/programaJava/ProgramaJava.java
@@ -0,0 +1,21 @@
+package programaJava;
+
+import programaJava.model.Mecanica;
+
+public class ProgramaJava {
+
+	public static void main(String[] args) {
+		
+		Mecanica mecanica = Mecanica();
+		
+		
+		
+		
+	}
+
+	private static Mecanica Mecanica() {
+		
+		return null;
+	}
+
+}
---
+++ src/programaJava/model/Mecanica.java
@@ -0,0 +1,62 @@
+package programaJava.model;
+
+public class Mecanica {
+	
+	private String email;
+	private String nomeCliente;
+	private String modelo;
+	private String placa;
+	private Double km;
+	
+	public Mecanica(String email, String nomeCliente, String modelo,
+			String placa, Double km) {
+		this.email = email;
+		this.nomeCliente = nomeCliente;
+		this.modelo = modelo;
+		this.placa = placa;
+		this.km = km;
+		
+		
+		
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public Double getKm() {
+		return km;
+	}
+
+	public void setKm(Double km) {
+		this.km = km;
+	}
+}
